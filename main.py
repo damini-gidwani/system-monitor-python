1759fb34be5016257192505c1088409bd85646d5
Damini Gidwani <daminigidwani13@gmail.com>
2026-07-25 20:52:59 +0530

    update code for platform independent

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import psutil as p
 import time
 import os
-from colorama import Fore , init
+from colorama import Fore, init
 import csv
 from datetime import datetime
 
@@ -10,20 +10,32 @@ session_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 CSV_FILE = f"logs/system_metrics_{session_time}.csv"
 
 
-with open(CSV_FILE, "w", newline="") as file:
-    writer = csv.writer(file)
-
-    writer.writerow([
+if os.name == "nt":
+    headers = [
         "Timestamp",
         "CPU (%)",
         "RAM (%)",
         "Disk C (%)",
         "Disk D (%)",
         "Upload (KB/s)",
-        "Download (KB/s)"
-    ])
+        "Download (KB/s)",
+    ]
+else:
+    headers = [
+        "Timestamp",
+        "CPU (%)",
+        "RAM (%)",
+        "Root (%)",
+        "Upload (KB/s)",
+        "Download (KB/s)",
+    ]
+
+with open(CSV_FILE, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(headers)
 
 init(autoreset=True)
+
 
 def get_color(percent):
     if percent < 50:
@@ -33,10 +45,12 @@ def get_color(percent):
     else:
         return Fore.RED
 
+
 def progress_bars(percent, length=20):
     filled = int((percent / 100) * length)
     empty = length - filled
     return "█" * filled + "░" * empty
+
 
 cpu_alert = 90
 ram_alert = 85
@@ -53,37 +67,40 @@ for proc in p.process_iter():
 time.sleep(1)
 try:
     while True:
-       
-       
-        os.system("cls")
+        os.system("cls" if os.name == "nt" else "clear")
 
         print(Fore.CYAN + "=" * 45)
         print(Fore.CYAN + f"{'SYSTEM MONITOR':^45}")
         print(Fore.CYAN + "=" * 45)
 
         cpu = p.cpu_percent(interval=None)
-        color=get_color(cpu)
+        color = get_color(cpu)
         print(f"CPU      {color}{progress_bars(cpu)}   {cpu:.1f}%\n")
-        if(cpu>=cpu_alert):
-           print(Fore.RED + "⚠ WARNING: High CPU Usage!\n")
+        if cpu >= cpu_alert:
+            print(Fore.RED + "⚠ WARNING: High CPU Usage!\n")
 
         ram = p.virtual_memory().percent
-        color=get_color(ram)
+        color = get_color(ram)
         print(f"RAM      {color}{progress_bars(ram)}   {ram:.1f}%\n")
-        if(ram>=ram_alert):
-           print(Fore.YELLOW + "⚠ WARNING: High RAM Usage!\n")
+        if ram >= ram_alert:
+            print(Fore.YELLOW + "⚠ WARNING: High RAM Usage!\n")
 
-        disk_c = p.disk_usage("C:\\").percent
-        color=get_color(disk_c)
-        print(f"DISK C   {color}{progress_bars(disk_c)}   {disk_c:.1f}%\n")
-        if disk_c >= disk_alert:
-         print(Fore.YELLOW + "⚠ WARNING: C Drive Almost Full!\n")
+        if os.name == "nt":
+            disk_c = p.disk_usage("C:\\").percent
+            color = get_color(disk_c)
+            print(f"DISK C   {color}{progress_bars(disk_c)}   {disk_c:.1f}%\n")
+            if disk_c >= disk_alert:
+                print(Fore.YELLOW + "⚠ WARNING: C Drive Almost Full!\n")
 
-        disk_d = p.disk_usage("D:\\").percent
-        color=get_color(disk_d)
-        print(f"DISK D   {color}{progress_bars(disk_d)}   {disk_d:.1f}%\n")
-        if disk_d >= disk_alert:
-         print(Fore.YELLOW + "⚠ WARNING: D Drive Almost Full!\n")
+            disk_d = p.disk_usage("D:\\").percent
+            color = get_color(disk_d)
+            print(f"DISK D   {color}{progress_bars(disk_d)}   {disk_d:.1f}%\n")
+            if disk_d >= disk_alert:
+                print(Fore.YELLOW + "⚠ WARNING: D Drive Almost Full!\n")
+        else:
+            storage = p.disk_usage("/").percent
+            color = get_color(storage)
+            print(f"ROOT   {color}{progress_bars(storage)}   {storage:.1f}%\n")
 
         new = p.net_io_counters()
         upload = (new.bytes_sent - old.bytes_sent) / kb / 5
@@ -103,10 +120,10 @@ try:
         processes = []
         for proc in p.process_iter(["pid", "name", "memory_percent"]):
             try:
-               if proc.info["name"] == "System Idle Process":
+                if proc.info["name"] == "System Idle Process":
                     continue
-               proc.info["cpu_percent"] = proc.cpu_percent()
-               processes.append(proc.info)
+                proc.info["cpu_percent"] = proc.cpu_percent()
+                processes.append(proc.info)
             except (p.NoSuchProcess, p.AccessDenied):
                 pass
 
@@ -123,20 +140,34 @@ try:
         print(Fore.MAGENTA + f"{'PID':<8}{'Process':<25}{'CPU %':<8}")
         for proc in top[:5]:
             print(f"{proc['pid']:<8}{proc['name']:<25}{proc['cpu_percent']:.2f}")
-            
+
         with open(CSV_FILE, "a", newline="") as file:
             writer = csv.writer(file)
 
-            writer.writerow([
-               datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-               round(cpu, 2),
-               round(ram, 2),
-               round(disk_c, 2),
-               round(disk_d, 2),
-               round(upload, 3),
-               round(download, 3)
-            ])
-         
+            if os.name == "nt":
+                writer.writerow(
+                    [
+                        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                        round(cpu, 2),
+                        round(ram, 2),
+                        round(disk_c, 2),
+                        round(disk_d, 2),
+                        round(upload, 3),
+                        round(download, 3),
+                    ]
+                )
+            else:
+                writer.writerow(
+                    [
+                        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                        round(cpu, 2),
+                        round(ram, 2),
+                        round(storage, 2),
+                        round(upload, 3),
+                        round(download, 3),
+                    ]
+                )
+
         time.sleep(5)
 
 except KeyboardInterrupt as e:
